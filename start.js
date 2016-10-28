b460f214ee491905d99b9ae56a38c18e67ac3167
fai516 <ysryb@mst.edu>
2016-10-27 19:14:18 -0500

    Update start.js

--- start.js
+++ start.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello Apple Pen!!\n');
 });
 
 server.listen(port, hostname, () => {
